2329c844e3694db33b2189860ad5d1158d57542b
peinorim <peinorim@gmail.com>
2013-10-22 11:48:19 +0200

    Metiers

---
+++ src/JdrCorp/ElricBundle/Entity/CompetenceMetier.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace JdrCorp\ElricBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * CompetenceMetier
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class CompetenceMetier {
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Id
+     * @ORM\ManyToOne(targetEntity="Sdz\JdrCorpElricBundle\Entity\Competence")
+     */
+    private $competence;
+
+    /**
+     * @ORM\Id
+     * @ORM\ManyToOne(targetEntity="Sdz\JdrCorpElricBundle\Entity\Metier")
+     */
+    private $metier;
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getCompetence() {
+        return $this->competence;
+    }
+
+    public function getMetier() {
+        return $this->metier;
+    }
+
+}
