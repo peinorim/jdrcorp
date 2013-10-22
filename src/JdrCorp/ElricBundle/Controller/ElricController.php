eda0412064518987ed06478c55dcc332e3c68c2f
peinorim <peinorim@gmail.com>
2013-10-22 11:05:44 +0200

    Metiers

--- src/JdrCorp/ElricBundle/Controller/ElricController.php
+++ src/JdrCorp/ElricBundle/Controller/ElricController.php
@@ -7,9 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class ElricController extends Controller {
 
     public function indexAction() {
-        $repository = $this->getDoctrine()
-                ->getManager()
-                ->getRepository('JdrCorpElricBundle:Competence');
+        $repository = $this->getDoctrine()->getManager()->getRepository('JdrCorpElricBundle:Competence');
         $listeComp = $repository->findAll();
         return $this->render('JdrCorpElricBundle:Elric:index.html.twig',array('listeComp' => $listeComp));
     }
