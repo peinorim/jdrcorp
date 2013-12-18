af31196e549109e17ff2a2ec592a1bf26be732aa
peinorim <peinorim@gmail.com>
2013-12-18 22:44:16 +0100

    Armes de jet

--- src/JdrCorp/ElricBundle/Entity/Image.php
+++ src/JdrCorp/ElricBundle/Entity/Image.php
@@ -21,14 +21,10 @@ class Image {
     }
     
     public function getUploadRootDir() {
-        // the absolute directory path where uploaded
-        // documents should be saved
         return __DIR__ . '/../../../../web/' . $this->getUploadDir();
     }
 
     protected function getUploadDir() {
-        // get rid of the __DIR__ so it doesn't screw up
-        // when displaying uploaded doc/image in the view.
         return 'uploads/avatars';
     }
 
