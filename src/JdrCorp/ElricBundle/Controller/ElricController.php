8adb7b4847348b8f71512983be5aac2743a59fee
peinorim <peinorim@gmail.com>
2014-01-10 16:53:18 +0100

    Pv + mana

--- src/JdrCorp/ElricBundle/Controller/ElricController.php
+++ src/JdrCorp/ElricBundle/Controller/ElricController.php
@@ -99,8 +99,7 @@ class ElricController extends Controller {
             }
             else
             {
-                return $this->render('JdrCorpElricBundle:Elric:createPerso.html.twig', array('perso' => $perso, 'myComp' => $perso->getCompetences(), 'mySorts' => $perso->getSorts(), 'listeComp' => $listeComp, 'image' => $avatar, 'myArmes' => $armes));
-                //return new Response($this->get('knp_snappy.pdf')->getOutputFromHtml($html), 200, array('Content-Type' => 'application/pdf'));
+                return new Response($this->get('knp_snappy.pdf')->getOutputFromHtml($html), 200, array('Content-Type' => 'application/pdf'));
             }
         }
     }
