cd954b81ac1202bb0cbe60dcf8a46eb3e941dbbb
peinorim <peinorim@gmail.com>
2014-01-28 16:36:42 +0100

    Guildes grille met/comp

--- src/JdrCorp/GuildesBundle/Controller/GuildesController.php
+++ src/JdrCorp/GuildesBundle/Controller/GuildesController.php
@@ -3,13 +3,65 @@
 namespace JdrCorp\GuildesBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class GuildesController extends Controller {
 
     public function indexAction() {
         $notice = null;
         $type = null;
-        return $this->render('JdrCorpGuildesBundle:Guildes:index.html.twig', array('notice' => $notice, 'type' => $type));
+        $em = $this->getDoctrine()->getManager('guildes');
+        $repositoryCarac = $em->getRepository('JdrCorpGuildesBundle:Carac');
+        $repositoryChap = $em->getRepository('JdrCorpGuildesBundle:Chapitre');
+        $repositoryComp = $em->getRepository('JdrCorpGuildesBundle:Competence');
+        $repositoryMetier = $em->getRepository('JdrCorpGuildesBundle:Metier');
+
+        $listeCarac = $repositoryCarac->findAll();
+        $listeChap = $repositoryChap->findAll();
+        $listeComp = $repositoryComp->findBy(array(), array('nom' => 'asc'));
+        $listeMetier = $repositoryMetier->findBy(array(), array('nom' => 'asc'));
+
+        return $this->render('JdrCorpGuildesBundle:Guildes:index.html.twig', array('notice' => $notice,
+                    'type' => $type,
+                    'listeCarac' => $listeCarac,
+                    'listeChap' => $listeChap,
+                    'listeComp' => $listeComp,
+                    'listeMetiers' => $listeMetier));
+    }
+
+    public function linksAction($type, $id) {
+
+        $em = $this->getDoctrine()->getManager('guildes');
+        $allCompMetier = null;
+        $allMetierComp = null;
+
+        if ($type === 'metier' && $id > 0) {
+            $metier = $em->getRepository('JdrCorpGuildesBundle:Metier')->find($id);
+            if ($metier === null) {
+                throw $this->createNotFoundException('Metier[id=' . $id . '] inexistant.');
+            } else {
+                $listeCompMetier = $em->getRepository('JdrCorpGuildesBundle:CompetenceMetier')->findByMetier($metier->getId());
+                foreach ($listeCompMetier as $compMetier) {
+                    $allCompMetier[$compMetier->getCompetence()->getId()] = $compMetier->getCompetence()->getNom();
+                }
+            }
+        } else if ($type === 'comp' && $id > 0) {
+            $comp = $em->getRepository('JdrCorpGuildesBundle:Competence')->find($id);
+            if ($comp === null) {
+                throw $this->createNotFoundException('Competence[id=' . $id . '] inexistante.');
+            } else {
+                $listeCompMetier = $em->getRepository('JdrCorpGuildesBundle:CompetenceMetier')->findByCompetence($comp->getId());
+                foreach ($listeCompMetier as $compMetier) {
+                    $allMetierComp[$compMetier->getMetier()->getId()] = $compMetier->getMetier()->getNom();
+                }
+            }
+        }
+
+
+        $response = new Response(json_encode(array('type' => $type, 'id' => $id, 'comp' => $allCompMetier, 'metier' => $allMetierComp)));
+        $response->headers->set('Content-Type', 'application/json');
+
+        return $response;
     }
 
 }
