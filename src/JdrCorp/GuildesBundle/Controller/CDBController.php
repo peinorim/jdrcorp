79ffc1091b13e1afd8d22c07e0fd0d3571097b34
peinorim <peinorim@gmail.com>
2014-02-04 17:16:26 +0100

    Guildes cdb maisons

--- src/JdrCorp/GuildesBundle/Controller/CDBController.php
+++ src/JdrCorp/GuildesBundle/Controller/CDBController.php
@@ -9,6 +9,21 @@ class CDBController extends Controller {
     public function cdbAction() {
         $notice = null;
         $type = null;
-        return $this->render('JdrCorpGuildesBundle:Guildes:cdb.html.twig', array('notice' => $notice, 'type' => $type));
+        $em = $this->getDoctrine()->getManager('guildes');
+        $repositoryCdbAll = $em->getRepository('JdrCorpGuildesBundle:CDBAll');
+        $repositoryMesAll = $em->getRepository('JdrCorpGuildesBundle:MESAll');
+        $repositoryCdbMaison = $em->getRepository('JdrCorpGuildesBundle:CDBMaison');
+        $repositoryMaison = $em->getRepository('JdrCorpGuildesBundle:Maison');
+        $listeCdball = $repositoryCdbAll->findAll();
+        $listeCdbMaison = $repositoryCdbMaison->findAll();
+        $listeMaison = $repositoryMaison->findAll();
+        $listeMesall = $repositoryMesAll->findAll();
+        return $this->render('JdrCorpGuildesBundle:Guildes:cdb.html.twig', array('notice' => $notice,
+                    'type' => $type,
+                    'listecdb_all' => $listeCdball,
+                    'listecdbmaison' => $listeCdbMaison,
+                    'listemaison' => $listeMaison,
+                    'listemes_all' => $listeMesall));
     }
+
 }
