70e7c8f7420e05cbbbb57537d6d4baa3dd7289a5
peinorim <peinorim@gmail.com>
2014-09-22 17:54:48 +0200

    Caracs pdf en cours

--- src/JdrCorp/GuildesBundle/Entity/Perso.php
+++ src/JdrCorp/GuildesBundle/Entity/Perso.php
@@ -63,6 +63,62 @@ class Perso {
      */
     private $desc;
 
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="agile", type="integer")
+     */
+    private $agile;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="fort", type="integer")
+     */
+    private $fort;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="res", type="integer")
+     */
+    private $res;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="obs", type="integer")
+     */
+    private $obs;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="charm", type="integer")
+     */
+    private $charm;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="ruse", type="integer")
+     */
+    private $ruse;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="savant", type="integer")
+     */
+    private $savant;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="tal", type="integer")
+     */
+    private $tal;
+
     public function __construct($request) {
         $this->setNom($request->request->get('nom'));
         $this->setSurnom($request->request->get('surnom'));
@@ -70,8 +126,25 @@ class Perso {
         $this->setAge($request->request->get('age'));
         $this->setSexe($request->request->get('sexe'));
         $this->setDesc($request->request->get('desc'));
+        $this->setAgile($request->request->get('agile'));
+        $this->setFort($request->request->get('fort'));
+        $this->setRes($request->request->get('res'));
+        $this->setObs($request->request->get('obs'));
+        $this->setCharm($request->request->get('charm'));
+        $this->setSavant($request->request->get('ruse'));
+        $this->setRuse($request->request->get('savant'));
+        $this->setTal($request->request->get('tal'));
 
         return $this;
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId() {
+        return $this->id;
     }
 
     /**
@@ -195,12 +268,163 @@ class Perso {
     }
 
     /**
-     * Get id
+     * Set agile
+     *
+     * @param integer $agile
+     * @return Perso
+     */
+    public function setAgile($agile) {
+        $this->agile = $agile;
+        return $this;
+    }
+
+    /**
+     * Get agile
      *
      * @return integer 
      */
-    public function getId() {
-        return $this->id;
+    public function getAgile() {
+        return $this->agile;
+    }
+
+    /**
+     * Set obs
+     *
+     * @param integer $obs
+     * @return Perso
+     */
+    public function setObs($obs) {
+        $this->obs = $obs;
+        return $this;
+    }
+
+    /**
+     * Get obs
+     *
+     * @return integer 
+     */
+    public function getObs() {
+        return $this->obs;
+    }
+
+    /**
+     * Set fort
+     *
+     * @param integer $fort
+     * @return Perso
+     */
+    public function setFort($fort) {
+        $this->fort = $fort;
+        return $this;
+    }
+
+    /**
+     * Get fort
+     *
+     * @return integer 
+     */
+    public function getFort() {
+        return $this->fort;
+    }
+
+    /**
+     * Set res
+     *
+     * @param integer $res
+     * @return Perso
+     */
+    public function setRes($res) {
+        $this->res = $res;
+        return $this;
+    }
+
+    /**
+     * Get res
+     *
+     * @return integer 
+     */
+    public function getRes() {
+        return $this->res;
+    }
+
+    /**
+     * Set charm
+     *
+     * @param integer $charm
+     * @return Perso
+     */
+    public function setCharm($charm) {
+        $this->charm = $charm;
+        return $this;
+    }
+
+    /**
+     * Get charm
+     *
+     * @return integer 
+     */
+    public function getCharm() {
+        return $this->charm;
+    }
+
+    /**
+     * Set ruse
+     *
+     * @param integer $ruse
+     * @return Perso
+     */
+    public function setRuse($ruse) {
+        $this->ruse = $ruse;
+        return $this;
+    }
+
+    /**
+     * Get ruse
+     *
+     * @return integer 
+     */
+    public function getRuse() {
+        return $this->ruse;
+    }
+
+    /**
+     * Set savant
+     *
+     * @param integer $savant
+     * @return Perso
+     */
+    public function setSavant($savant) {
+        $this->savant = $savant;
+        return $this;
+    }
+
+    /**
+     * Get savant
+     *
+     * @return integer 
+     */
+    public function getSavant() {
+        return $this->savant;
+    }
+
+    /**
+     * Set tal
+     *
+     * @param integer $tal
+     * @return Perso
+     */
+    public function setTal($tal) {
+        $this->tal = $tal;
+        return $this;
+    }
+
+    /**
+     * Get tal
+     *
+     * @return integer 
+     */
+    public function getTal() {
+        return $this->tal;
     }
 
 }
